3762eea803f41c53ca667fda48fea399cc95d173
Alexandre Kolobov <kolobovalexandre@gmail.com>
2023-02-01 17:02:16 +0100

    Flake8 corrections

--- books_scraping.py
+++ books_scraping.py
@@ -5,57 +5,53 @@ import csv
 import os
 import shutil
 
-#Variables globales:
+
+# Global variables:
 url_racine = "http://books.toscrape.com"
-#url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-url_category_to_scrap = "http://books.toscrape.com/catalogue/category/books/mystery_3/page-1.html"
-url_category = [] #liste qui va contenir url de chaque category
-url = [] #liste qui va contenir url de chaque livre
-liste_dict_categ = [] #liste qui va contenir les dictionnaires avec des info de chaque livre par categorie
+url_category = []  # List for urls of each category
+url = []  # List for url of each book
+liste_dict_categ = []  # List to save dictionnaries with all book information
+folder_cvs = "scraping_result/"
 
-#Remove old files with folder and create a new empty folder
-if os.path.exists("scraping_result/") == True:
-    shutil.rmtree("scraping_result/")
-
-os.mkdir("scraping_result/")
+# Remove old files and folder and create a new empty folder:
+if os.path.exists(folder_cvs) is True:
+    shutil.rmtree(folder_cvs)
+os.mkdir(folder_cvs)
 
 
-#Verifier que l'URL est accessible
-"""
-if response.ok:
-    print ("Le lien " + url + " est accessible")
-    print ("Le scrapping des informations est en cours..")
-else:
-    print("L'url: " + url + " n'est pas accessible")
-    exit(1)
-"""
-
-def list_categories():
+def list_categories():  # To get URL of each category
     response = requests.get(url_racine)
-    soup = BeautifulSoup(response.text ,features="html.parser")
-    all_categories = soup.find("ul" , {"class" : "nav nav-list"}).find("ul").findAll("a")
+
+    # Check that URL is ok:
+    if response.ok:
+        print("The link: " + url_racine + " is ok")
+        print("Start scraping..")
+    else:
+        print("The link: " + url_racine + " is not accessible")
+        exit(1)
+
+    # Put the URL of each category in the list
+    soup = BeautifulSoup(response.text, features="html.parser")
+    all_categories_navlist = soup.find("ul", {"class": "nav nav-list"})
+    all_categories = all_categories_navlist.find("ul").findAll("a")
 
     for category_a in all_categories:
         global url_category
         url_category.append(url_racine + "/" + category_a["href"])
 
- 
 
-
-def category_information(url_category):
+def category_information(url_category):  # To get URL of each book
     response = requests.get(url_category)
+    soup = BeautifulSoup(response.text, features="html.parser")
 
-
-    soup = BeautifulSoup(response.text ,features="html.parser")
-
-    one_category_url = soup.findAll("div" , {"class" : "image_container"})
+    one_category_url = soup.findAll("div", {"class": "image_container"})
     for html_a in one_category_url:
         a = html_a.find("a")
         global url
-        url.append(url_racine + a["href"].replace("../../..","/catalogue"))
+        url.append(url_racine + a["href"].replace("../../..", "/catalogue"))
 
-    #On utilise une fonction recursive pour traiter l'url suivant s'il existe
-    find_next_page = soup.find("li",{"class" : "next"})
+    # Use of recursive to check possibility of next page
+    find_next_page = soup.find("li", {"class": "next"})
 
     if str(find_next_page) != "None":
         find_next_page_a = find_next_page.find("a")
@@ -71,97 +67,81 @@ def category_information(url_category):
         category_information(url_next_page)
 
 
-
 def extract_info(book_url):
     response = requests.get(book_url)
-    soup = BeautifulSoup(response.text ,features="html.parser")
-    all_info = soup.find("table" , {"class" : "table table-striped"}).findAll("td")
+    soup = BeautifulSoup(response.text, features="html.parser")
+    all_info_tstriped = soup.find("table", {"class": "table table-striped"})
+    all_info = all_info_tstriped.findAll("td")
 
     upc = all_info[0].text
-    #product_type = all_info[1].text
-    price_excluding_tax = all_info[2].text[2:] #Faut-il garder symbole livres ?
-    price_including_tax = all_info[3].text[2:]
-    #tax = all_info[4].text
-    number_available = re.findall("\d+",all_info[5].text) #Le format semble être toujour le même, donc cet extraction doit fonctionner
-    review_rating = all_info[6].text  #Number of reviews or stars? 
+    # product_type = all_info[1].text
+    price_excluding_tax = all_info[2].text[2:]  # To remove the currency symbol
+    price_including_tax = all_info[3].text[2:]  # To remove the currency symbol
+    # tax = all_info[4].text
+    # d+ means number in range 0-9 (one or more times):
+    number_available = re.findall("\\d+", all_info[5].text)
+    review_rating = all_info[6].text
 
-
-    title_info = soup.find("div" , {"class" : "col-sm-6 product_main"}).find("h1")
+    title_info_div = soup.find("div", {"class": "col-sm-6 product_main"})
+    title_info = title_info_div.find("h1")
     title = title_info.text
 
-    #Cette descritpion ne contient pas d'attribut
-    product_description_info = soup.find("article", {"class" : "product_page"}).find("p", {"class" : None})
-    if str(product_description_info) == "None":
+    # This description doesn't have class, we can use None
+    item_info_article = soup.find("article", {"class": "product_page"})
+    item_info_p = item_info_article.find("p", {"class": None})
+
+    if str(item_info_p) == "None":
         product_description = "Pas de description"
     else:
-        product_description = product_description_info.text
+        product_description = item_info_p.text
 
-    #L'url de la category est unique et contient une part variable specifique à chaque category
-    category_info = soup.find("a",{"href" : re.compile("\.\./category/books/[A-Za-z0-9_ -]+/index\.html")})
+    # URL of each category is only one element different from others
+    category_info = soup.find("a", {"href": re.compile("\\.\\./category/books/"
+                              "[A-Za-z0-9_ -]+/index\\.html")})
     category = category_info.text
-    # A optimiser 
-
 
     image_url_info = soup.find("img")
-    image_url = url_racine + image_url_info["src"].replace("../..","") #image_url_info["src"] - You can access a tag’s attributes by treating the tag like a dictionary
+    image_url = url_racine + image_url_info["src"].replace("../..", "")
 
+    # Add all book's information in a dictionnary
+    dict_info = {"product_page_url": book_url, "universal_product_code": upc,
+                 "title": title, "price_including_tax": price_including_tax,
+                 "price_excluding_tax": price_excluding_tax,
+                 "number_available": number_available[0],
+                 "product_description": product_description,
+                 "category": category,
+                 "review_rating": review_rating,
+                 "image_url": image_url}
 
-    #Ecriture dans un fichier 
-    #
-    """
-    with open ("scraping_result.csv","a", encoding="utf-8") as my_file:
-        my_file.write ("product_page_url;universal_product_code;title;price_including_tax;price_excluding_tax;number_available;product_description;category;review_rating;image_url" + "\n")
-        my_file.write (book_url + ";" + upc + ";" + title + ";" + price_including_tax + ";" + price_excluding_tax + ";" + number_available[0] + ";" + product_description + ";" + category + ";" + review_rating + ";" + image_url + "\n")
-    """
-    #Passer par dictionnaire + une fonction qui fait le liste des dictionnaires
-   
-    dict_info = {"product_page_url":book_url,"universal_product_code":upc,"title":title,"price_including_tax":price_including_tax, \
-        "price_excluding_tax":price_excluding_tax,"number_available":number_available[0],"product_description":product_description, \
-        "category":category,"review_rating":review_rating,"image_url":image_url}
-    
+    # Add dictionnary in a list
     liste_dict_categ.append(dict_info)
-    
 
 
-
+# Create and append a file per category with book's informations
 def write_file(list_with_dicts):
     for i in list_with_dicts:
-        print (i["category"])
-        if os.path.isfile("scraping_result/" + i["category"] + ".csv") == False:
-            with open ("scraping_result/"+ i["category"] + ".csv","w",newline="",encoding="utf-8") as my_file:
-                writer = csv.DictWriter(my_file, i.keys(),delimiter=";")
+        file_name = (folder_cvs + i["category"] + ".csv")
+        if os.path.isfile(folder_cvs + i["category"] + ".csv") is False:
+            with open(file_name, "w", newline="", encoding="utf-8") as file:
+                writer = csv.DictWriter(file, i.keys(), delimiter=";")
                 writer.writeheader()
                 writer.writerow(i)
         else:
-            with open ("scraping_result/"+ i["category"] + ".csv","a",newline="",encoding="utf-8") as my_file:
-                writer = csv.DictWriter(my_file, i.keys(),delimiter=";")
+            with open(file_name, "a", newline="", encoding="utf-8") as file:
+                writer = csv.DictWriter(file, i.keys(), delimiter=";")
                 writer.writerow(i)
 
 
+print("Listing categories")
 list_categories()
 
+print("Getting books URL")
 for category in url_category:
     category_information(category)
-#category_information("http://books.toscrape.com/catalogue/category/books/music_14/index.html")
 
+print("Getting books informations")
 for i in url:
     extract_info(i)
-    #data = extract_info(i)
-    #liste_categ.append(data)
-    #dev fonction pour ecrire un fichier en one shot 
 
+print("Making files")
 write_file(liste_dict_categ)
-
-
-
-"""
-with open ("scraping_result/" + category_a.text + ".csv","w") as my_file:
-    my_file.write ("product_page_url;universal_product_code;title;price_including_tax;price_excluding_tax;number_available;product_description;category;review_rating;image_url" + "\n")
-    now = datetime.now()
- 
-print("now =", now)
-
-# dd/mm/YY H:M:S
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-print("date and time =", dt_string)
-"""
